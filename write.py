c5cc160152e5d52efc6a41da95ea90aa6f6909bb
Sungjin Kang <ujuc@ujuc.kr>
2015-04-13 00:10:30 +0900

    edit whitespace

--- write.py
+++ write.py
@@ -22,27 +22,28 @@ TEMPLATE = """
 
 file_path = "content/blog"
 
+
 def make_entry(title):
-	today = datetime.today()
-	slug = title.lower().strip().replace(' ', '-')
-	file_create = "{0}/{1}-{2:0>2}-{3:0>2}-{4}.rst".format( file_path,
-		today.year, today.month, today.day, slug)
-	t = TEMPLATE.strip().format(
-		title=title,
-		hashes='#' * len(title),
-		year=today.year,
-		month=today.month,
-		day=today.day,
-		hour=today.hour,
-		minute=today.minute,
-		slug=slug)
-	with open(file_create, 'w') as w:
-		w.write(t)
-	print("File created -> " + file_create)
+    today = datetime.today()
+    slug = title.lower().strip().replace(' ', '-')
+    file_create = "{0}/{1}-{2:0>2}-{3:0>2}-{4}.rst".format( file_path,
+        today.year, today.month, today.day, slug)
+    t = TEMPLATE.strip().format(
+        title=title,
+        hashes='#' * len(title),
+        year=today.year,
+        month=today.month,
+        day=today.day,
+        hour=today.hour,
+        minute=today.minute,
+        slug=slug)
+    with open(file_create, 'w') as w:
+        w.write(t)
+    print("File created -> " + file_create)
 
 
 if __name__ == '__main__':
-	if len(sys.argv) > 1:
-		make_entry(sys.argv[1])
-	else:
-		print("No title given")
+    if len(sys.argv) > 1:
+        make_entry(sys.argv[1])
+    else:
+        print("No title given")
