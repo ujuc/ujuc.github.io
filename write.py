a4d57f870aa5da85cd9229f99b0ab5898618e29b
Sungjin Kang <ujuc@ujuc.kr>
2015-12-26 01:09:52 +0900

    Add markdown template
    * Markdown 을 구성할 수 있도록 하나더 추가

--- write.py
+++ write.py
@@ -2,58 +2,75 @@
 # coding: utf-8
 # Oirign : http://nafiulis.me/making-a-static-blog-with-pelican.html
 # edit: ujuc
-'''write
+"""write
 
 Usage:
-    write new [-p | --page] <title>
+    write new [-m | --markdown] [-p | --page] <title>
     write edit <title>
     write backup <title> <date>
 
 -d --date       datetime change
 
-'''
-
-
-
-import sys
-from docopt import docopt
-from datetime import datetime
-
-TEMPLATE = """
-{title}
-{hashes}
-
-{date}
-:category:
-:tags:
-:slug: {slug}
-:summary:
-{modified}
-
 """
+
+from datetime import datetime
+from docopt import docopt
+
+RST_TEMPLATE = ('{title}\n'
+                '{hashes}\n'
+                '\n'
+                '{date}\n'
+                ':category: \n'
+                ':tags: \n'
+                ':slug: {slug}\n'
+                ':summary: \n'
+                '{modified}\n'
+                '\n')
+
+MD_TEMPLATE = ('Title: {title}\n'
+               'Date: {date}\n'
+               'Category: \n'
+               'Tags: \n'
+               'Slug: {slug}\n'
+               'Summary: \n')
 
 post_path = "content/blog"
 page_path = "content/pages"
 
-def make_entry(title, path):
+
+def make_entry(title, path, template):
     today = datetime.today()
     # GTODO : 한국어를 영어로 변환하는 것이 필요
     slug = title.lower().strip().replace(' ', '-')
-    file_create = "{0}/{1}-{2:0>2}-{3:0>2}-{4}.rst".format(path,
-                                                           today.year,
-                                                           today.month,
-                                                           today.day, slug)
     date = ":date: {year}-{month:02d}-{day:02d} {hour:02d}:{minute:02d}"\
         .format(year=today.year, month=today.month, day=today.day,
                 hour=today.hour, minute=today.minute)
-    t = TEMPLATE.strip().format(
-        title=title,
-        hashes='#' * len(title) * 2,
-        slug=slug,
-        date=date,
-        modified="")
+
+    if template == 'rst':
+        file_create = \
+            "{0}/{1}-{2:0>2}-{3:0>2}-{4}.rst".format(path,
+                                                     today.year,
+                                                     today.month,
+                                                     today.day, slug)
+        article = RST_TEMPLATE.strip().format(
+                title=title,
+                hashes='#' * len(title) * 2,
+                slug=slug,
+                date=date,
+                modified="")
+    elif template == 'md':
+        file_create = \
+            "{0}/{1}-{2:0>2}-{3:0>2}-{4}.md".format(path,
+                                                    today.year,
+                                                    today.month,
+                                                    today.day, slug)
+        article = MD_TEMPLATE.strip().format(
+                title=title,
+                slug=slug,
+                date=date)
+
     with open(file_create, 'w') as w:
-        w.write(t)
+        w.write(article)
     print("File created -> " + file_create)
 
 
@@ -69,22 +86,17 @@ def edit_entry(title):
 
 if __name__ == '__main__':
     opt = docopt(__doc__, version='write 0.1')
-    print(opt)
 
     if opt['new']:
-        print("{0}, {1}, {2}, {3}".
-              format(opt["new"], opt["-p"], opt["--page"], opt["<title>"]))
         if opt["-p"] or opt["--page"]:
             make_entry(opt["<title>"], page_path)
-            print("page")
-        make_entry(opt["<title>"], post_path)
+        elif opt["-m"] or opt["--markdown"]:
+            make_entry(opt["<title>"], post_path, 'md')
+        else:
+            make_entry(opt["<title>"], post_path, 'rst')
     elif opt['edit']:
         print("edit")
     elif opt["backup"]:
         print("backup")
     else:
         print(opt)
-    # if len(sys.argv) > 1:
-    #     make_entry(sys.argv[1])
-    # else:
-    #     print("No title given")
