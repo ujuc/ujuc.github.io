6671a40ba97ae5139c28a36e68c2f9c96df8e1ab
Thomas Sungjin Kang <ujuc@ujuc.kr>
2020-02-09 13:38:18 +0900

    feat: publish 구성에 meta image 위치를 변수로 등록한다
    - 변수로 등록해놔야 편하니 변수로 변경하였다.

--- publishconf.py
+++ publishconf.py
@@ -12,6 +12,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 SITEURL = "https://ujuc.github.io"
+META_IMAGE = "img/main.jpg"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
